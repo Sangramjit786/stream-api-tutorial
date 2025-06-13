b0cfa988a3bdd3ea65a1dfe1c50eab7ea17f924c
Rohit <nathrohit48@gmail.com>
2025-06-13 08:19:48 +0530

    Stream-API commit for creation of streams

---
+++ src/main/java/net/javaguides/StreamDemo.java
@@ -0,0 +1,55 @@
+package net.javaguides;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamDemo {
+    public static void main(String[] args) {
+        // feature intoduced in java 8
+        // process collection of data in a fuctional and declarative manner
+        // Simplify Data Processing
+        // Embrace Functional Programming
+        // Improve Readability and Maintainability
+        // Enable Easy Paralleism
+
+        // What is stream?
+        // a sequence of elements supporting functional and declarative programming
+
+        // How to Use Streams?
+        // Source, intermediate operations & terminal operation
+
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5); // Source
+
+        // Find count of even numbers
+        //traditional way
+        int count = 0;
+        for (int i : numbers) {
+            if ((i & 1) == 0) {
+                count++;
+            }
+        }
+
+        System.out.println("traditional way: " + count);
+
+        // Using Stream
+        System.out.println("Stream way: " + numbers.stream().filter(x -> (x & 1) == 0).count());
+
+
+        //// Creating Streams -> different ways, from collections and infinite streams
+        // 1. From Collections(it is applicable for any collection, for example we have taken 'List')
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        Stream<Integer> stream = list.stream();
+        // 2. From Arrays
+        String[] array = {"a", "b", "c"};
+        Stream<String> stream1 = Arrays.stream(array);
+        // 3. Using Stream.of()
+        Stream<String> stream2 = Stream.of("a", "b");
+        // 4. Infinite streams(we use limit(), for finite size)
+        Stream<Integer> gnerate = Stream.generate(() -> 1);
+        Stream.iterate(1, x -> x + 1);
+
+
+
+    }
+}
