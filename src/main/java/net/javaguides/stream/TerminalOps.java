734646c104f5f514c6d88eedf1603478a2174d9a
Rohit <nathrohit48@gmail.com>
2025-06-15 08:34:46 +0530

    Stream-API commit for more intermediate and terminal operations.

--- src/main/java/net/javaguides/stream/TerminalOps.java
+++ src/main/java/net/javaguides/stream/TerminalOps.java
@@ -1,10 +1,8 @@
 package net.javaguides.stream;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TerminalOps {
     public static void main(String[] args) {
@@ -50,14 +48,32 @@ public class TerminalOps {
         System.out.println(list.stream().findFirst().get());
         System.out.println(list.stream().findAny().get());
 
+        // 7. toArray()
+        Object[] array = Stream.of(1, 2, 3).toArray();
+
+        // 8. min / max
+        System.out.println("max: " + Stream.of(2, 44, 69).max(Comparator.naturalOrder()).get());
+
+        System.out.println("max: " + Stream.of(2, 44, 69).max((o1, o2) -> o2 -o1));
+        /*here we are using comparator */
+
+        System.out.println("min: " + Stream.of(2, 44, 69).min(Comparator.naturalOrder()).get());
+
+        // 9. forEachOrdered
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        System.out.println("Using forEach with parallel stream:");
+        numbers.parallelStream().forEach(System.out::println);
+        System.out.println("Using forEachOrdered with parallel stream:");
+        numbers.parallelStream().forEachOrdered(System.out::println);
+
         // Examples: Filtering and Collecting Names
         List<String> names = Arrays.asList("Anna", "Bob", "Charlie", "David");
 
         System.out.println(names.stream().filter(x -> x.length() > 3).collect(Collectors.toList()));
 
         // Examples: Squaring and Sorting Numbers
-        List<Integer> numbers = Arrays.asList(5, 2, 9, 1, 6);
-        System.out.println(numbers.stream().map(x -> x * x).sorted().toList());
+        List<Integer> numbers1 = Arrays.asList(5, 2, 9, 1, 6);
+        System.out.println(numbers1.stream().map(x -> x * x).sorted().toList());
 
         // Examples: Summing Values
         List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5);
@@ -67,8 +83,13 @@ public class TerminalOps {
         String sentence = "Hello world";
         System.out.println(sentence.chars().filter(x -> x == 'l').count());
 
+
+        // Example
+        // Streams cannot be reused after a terminal operation has been called
+        Stream<String> stream = names.stream();
+        stream.forEach(System.out::println);
+//        List<String> list1 = stream.map(String::toUpperCase).toList(); //exception
+
         // stateful & stateless
-
-
     }
 }
